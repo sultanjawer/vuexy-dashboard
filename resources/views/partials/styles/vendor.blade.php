bf8eac53f74a96a702ae205e170e3bde205e751c
proamrakram <amroaarsi@gmail.com>
2023-01-02 15:32:20 +0200

    new-update

--- resources/views/partials/styles/vendor.blade.php
+++ resources/views/partials/styles/vendor.blade.php
@@ -59,3 +59,7 @@
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <x-livewire-alert::scripts />
+
+
+
+
