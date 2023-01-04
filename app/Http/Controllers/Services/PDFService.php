f14c83d6bedb6cc579f722a9be82b382d25900aa
proamrakram <amroaarsi@gmail.com>
2023-01-04 17:04:30 +0200

    new-update

--- app/Http/Controllers/Services/PDFService.php
+++ app/Http/Controllers/Services/PDFService.php
@@ -70,8 +70,8 @@ class PDFService extends Controller
             'procEnv' => [
                 'LANG' => 'ar_SA.utf-8',
             ],
-            'command' => 'C:\Program Files (x86)\PDFtk\bin\pdftk.exe',
-            'useExec' => true,
+            // 'command' => 'C:\Program Files (x86)\PDFtk\bin\pdftk.exe',
+            // 'useExec' => true,
         ]);
 
         $result = $pdf->fillForm($fillable)
