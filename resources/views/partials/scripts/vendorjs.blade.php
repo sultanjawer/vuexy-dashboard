ed59d9cffd6d392b2425866f0fbf217d9f50f136
proamrakram <amroaarsi@gmail.com>
2023-01-04 17:07:57 +0200

    new-update

--- resources/views/partials/scripts/vendorjs.blade.php
+++ resources/views/partials/scripts/vendorjs.blade.php
@@ -29,7 +29,7 @@
 
 <!-- BEGIN: Theme JS-->
 <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
- <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
+<script src="{{ asset('app-assets/js/core/app.js') }}"></script>
 <!-- END: Theme JS-->
 
 <!-- BEGIN: Page JS-->
